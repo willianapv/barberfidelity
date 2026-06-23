f8fd2ba2d6f5072dc0e0d847536f6e18874aad3d
willianapv <willian.apv@gmail.com>
2026-06-22 21:19:00 -0300

    Update App.py

--- App.py
+++ App.py
@@ -1,10 +1,13 @@
-from cliente import Cliente
+class Cliente:
+    def __init__(self, nome, telefone):
+        self.nome = nome
+        self.telefone = telefone
+        self.pontos = 0
 
-cliente1 = Cliente(
-    "Willian Almeida",
-    "(41)99999-9999"
-)
+    def adicionar_pontos(self, quantidade):
+        self.pontos += quantidade
 
-cliente1.adicionar_pontos(10)
-
-cliente1.exibir_dados()
+    def exibir_dados(self):
+        print(f"Nome: {self.nome}")
+        print(f"Telefone: {self.telefone}")
+        print(f"Pontos: {self.pontos}")
